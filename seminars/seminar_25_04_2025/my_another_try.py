164a644fea8aaba6e6d11db51bf106396331cf09
Anna Solovyova <solovy0va-aa@mail.ru>
2025-05-16 00:29:19 +0300

    seminar is deleted

--- seminars/seminar_25_04_2025/my_another_try.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-from datetime import datetime
-
-if __name__ == '__main__':
-    base_dir = pathlib.Path(__file__).parent
-    file_txt = base_dir / 'file.txt'
-    if not file_txt.exists():
-        file_txt.write_text('File content')
