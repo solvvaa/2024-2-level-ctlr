206aace83b216111464233970e9a93a9ece83b18
Anna Solovyova <solovy0va-aa@mail.ru>
2025-05-16 00:12:19 +0300

    changes

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -28,6 +28,7 @@ class Config:
     Class for unpacking and validating configurations.
     """
 
+
     def __init__(self, path_to_config: pathlib.Path) -> None:
         """
         Initialize an instance of the Config class.
