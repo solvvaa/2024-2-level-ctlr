a491b7d6d77952dc55b39463563891bb6d8fef5e
Anna Solovyova <solovy0va-aa@mail.ru>
2025-05-26 20:03:53 +0300

    changes

--- seminars/seminar_05_16_2025/try_udpipe.py
+++ seminars/seminar_05_16_2025/try_udpipe.py
@@ -17,6 +17,7 @@ from pathlib import Path
 try:
     import spacy
     import spacy_udpipe
+    from spacy_conll import ConllParser # type: ignore [import-not-found, import-untyped]
 except ImportError:
     print("No libraries installed. Failed to import.")
 
